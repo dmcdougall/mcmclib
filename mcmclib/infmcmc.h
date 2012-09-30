ee26b1b965a1afd872b0b525372be6baf09a7b87
Damon McDougall <damon.mcdougall@gmail.com>
2012-10-01 00:00:35 +0100

    Update documentation for functions and struct

--- mcmclib/infmcmc.h
+++ mcmclib/infmcmc.h
@@ -12,6 +12,10 @@
 #include <fftw3.h>
 #include <gsl/gsl_rng.h>
 
+/**
+ * \struct _mcmc_infchain
+ * \brief The Markov chain data structure
+ */
 struct _mcmc_infchain {
   /**
    * Number of degrees of freedom (Fourier coefficients) in the x direction.
@@ -132,60 +136,280 @@ struct _mcmc_infchain {
    * Stores the square of the L2 norm of the current Markov chain state.
    */
   double current_state_L2norm2;
-  /**
-   * Private
-   */
   fftw_plan _c2r;
-  /**
-   * Private
-   */
   fftw_plan _r2c;
-  /**
-   * Private
-   */
   gsl_rng *r;
-  /**
-   * Private
-   */
   double _short_time_acc_prob_avg;
-  /**
-   * Private
-   */
   double _bLow;
-  /**
-   * Private
-   */
   double _bHigh;
-  /**
-   * Private
-   */
   double *_M2;
 };
 
 typedef struct _mcmc_infchain mcmc_infchain;
 
+/**
+ * \brief Initialise an infinite dimensional Markov chain.
+ *
+ * The Markov Chain is assumed to live on a unifromly gridded square with
+ * periodic boundary conditions. These assumptions allow fast tranforms between
+ * the physical- and coefficient-domains.
+ *
+ * This function needs to be called before any other mcmclib function. It
+ * allocates memory needed by some of the outher functions.
+ *
+ * \param chain
+ * The chain to initialise.
+ *
+ * \param nj
+ * Number of Fourier coefficients in the x direction.
+ *
+ * \param nk
+ * Number of Fourier corfficients in the y direction.
+ */
 void mcmc_init_infchain(mcmc_infchain *chain, const int nj, const int nk);
+
+/**
+ * \brief Free a previously initialised Markov chain
+ *
+ * This frees all the memory allocated by ::mcmc_init_infchain.
+ *
+ * \param chain
+ * The chain to free.
+ */
 void mcmc_free_infchain(mcmc_infchain *chain);
+
+/**
+ * \brief Re-initialise all heap-allocated memory in the given chain.
+ *
+ * This is equivalent to a call to ::mcmc_free_infchain followed by a call to
+ * ::mcmc_init_infchain.
+ *
+ * \param chain
+ * The chain to reset.
+ */
 void mcmc_reset_infchain(mcmc_infchain *chain);
+
+/**
+ * \brief Make a standard random-walk proposal.
+ *
+ * \param chain
+ * The chain to make the proposal.
+ */
 void mcmc_propose_RWMH(mcmc_infchain *chain);
+
+/**
+ * \brief Update the chain after a proposal has been made.
+ *
+ * Given a chain that has made a proposal using ::mcmc_propose_RWMH and the
+ * value of the log-likelihood at the proposed state, update the current state
+ * of the chain and its moments.
+ *
+ * \param chain
+ * The chain to update.
+ *
+ * \param logLHDOfProposal
+ * The value of the log-likelihood functional using
+ * _mcmc_infchain::proposed_physical_state as input.
+ */
 void mcmc_update_RWMH(mcmc_infchain *chain, double logLHDOfProposal);
+
+/**
+ * \brief Seed the current state of the Markov chain with a random draw from
+ * the prior distribution.
+ *
+ * \param chain
+ * The chain to seed
+ */
 void mcmc_seed_with_prior(mcmc_infchain *chain);
+
+/**
+ * \brief Write chain information to disk.
+ *
+ * This only writes the _mcmc_infchain::nj and _mcmc_infchain::nk
+ * parameters to disk.
+ *
+ * \param chain
+ * The chain whose information is to be written to disk.
+ *
+ * \param fp
+ * The file pointer to write to.
+ */
 void mcmc_write_infchain_info(const mcmc_infchain *chain, FILE *fp);
+
+/**
+ * \brief Write out two infinite dimensional Markov chains (a vector field)
+ *
+ * \param U
+ * The horizontal component of the vector field
+ *
+ * \param V
+ * The vertical component of the vector field
+ *
+ * \param fp
+ * The file pointer to write the data to
+ */
 void mcmc_write_vectorfield_infchain(const mcmc_infchain *U, const mcmc_infchain *V, FILE *fp);
+
+/**
+ * \brief Write out Markov chain data to disk
+ *
+ * \param chain
+ * The chain to write to disk
+ *
+ * \param fp
+ * The file pointer to write the data to
+ */
 void mcmc_write_infchain(const mcmc_infchain *chain, FILE *fp);
+
+/**
+ * \brief Print some diagnostic chain information to the screen.
+ *
+ * \param chain
+ * The chain whose information is printed to the screen.
+ */
 void mcmc_print_infchain(mcmc_infchain *chain);
+
+/**
+ * \brief Set the random-walk proposal step size of a Markov chain
+ *
+ * \param chain
+ * The chain whose random-walk proposal step size is altered
+ *
+ * \param beta
+ * The step size to change to
+ */
 void mcmc_set_RWMH_stepsize(mcmc_infchain *chain, double beta);
+
+/**
+ * \brief Adapt the random-walk proposal of a Markov chain
+ *
+ * Adapt the Markov chain's random-walk proposal step size. It is adapted by
+ * either +inc or -inc depening on whether a current short-time average admits
+ * an acceptance rate that is too high or too low, respectively.
+ *
+ * 'Too high' is currently hard-coded at more than 30%. 'Too low' is hard-coded
+ * at less than 20%.
+ *
+ * Note: altering a Markov chain's proposal distribution destroys the Markovian
+ * property of the chain. Ergodicity is no longer guaranteed under these
+ * circumstances. Use this function with care.
+ *
+ * \param chain
+ * The chain to adapt
+ *
+ * \param inc
+ * The increment to apply to the chain
+ */
 void mcmc_adapt_RWMH_stepsize(mcmc_infchain *chain, double inc);
+
+/**
+ * \brief Set the fractional power of the prior covariance operator.
+ *
+ * The prior covariance operator is assumed to be the inverse of the Laplacian
+ * operator.
+ *
+ * \param chain
+ * The chain to apply the change to
+ *
+ * \param alpha
+ * The power to which the inverse Laplacian is raised
+ */
 void mcmc_set_prior_alpha(mcmc_infchain *chain, double alpha);
+
+/**
+ * \brief Set the Markov chain's prior covariance coefficient
+ *
+ * A larger prior covariance implies a less informative prior distribution.
+ * A smaller prior covariance implies a more informative prior.
+ *
+ * \param chain
+ * The chain to apply the change to
+ *
+ * \param var
+ * The multiplicative coefficient of the prior covariance operator
+ */
 void mcmc_set_prior_var(mcmc_infchain *chain, double var);
+
+/**
+ * \brief Compute the square of the L2 norm of the current chain state
+ *
+ * \param chain
+ * The chain whose current state is used to compute the norm
+ *
+ * \return
+ * The L2 norm of the current state of the chain
+ */
 double mcmc_current_L2(mcmc_infchain *chain);
+
+/**
+ * \brief Compute the square of the L2 norm of the proposed chain state
+ *
+ * \param chain
+ * The chain whose proposed state is used to compute the norm
+ *
+ * \return
+ * The L2 norm of the proposed state of the chain
+ */
 double mcmc_proposed_L2(mcmc_infchain *chain);
+
+/**
+ * \brief Compute the square of the L2 norm of the currently stored prior draw
+ *
+ * \param chain
+ * The chain whose prior draw state is used to compute the norm
+ *
+ * \return
+ * The L2 norm of the currently stored prior draw
+ */
 double mcmc_prior_L2(mcmc_infchain *chain);
 
+/**
+ * \brief Seed two Markov chains with divergence free vector field
+ *
+ * After calling this function, the current state of chain1 and the current
+ * state of chain 2 form a vector field. This vector field will be divergence
+ * free.
+ *
+ * \param chain1
+ * The chain that will hold the horizontal component of the vector field
+ *
+ * \param chain2
+ * The chain that will hold the vertical component of the vector field
+ */
 void mcmc_seed_with_divfree_prior(mcmc_infchain *chain1, mcmc_infchain *chain2);
+
+/**
+ * \brief Propose a divergence free vector field
+ *
+ * After calling this function, the proposed state of chain1 and chain2 will
+ * describe a vector field whose divergence is zero.
+ *
+ * \param chain1
+ * The chain to hold the horizontal component
+ *
+ * \param chain2
+ * The chain to hold the vertical component
+ */
 void mcmc_propose_divfree_RWMH(mcmc_infchain *chain1, mcmc_infchain *chain2);
+
+/**
+ * \brief Update a divergence free vector field using random-walk
+ * Metropolis-Hastings
+ *
+ * Given the value of the log-likelihood at proposed vector field, update the
+ * current state of both chains
+ *
+ * TODO: Add information about what members are updated
+ *
+ * \param chain1
+ * The horizontal component of the vector field
+ *
+ * \param chain2
+ * The vertical component of the vector field
+ *
+ * \param logLHDOfProposal
+ * The value of the log-likelihood at the proposed vector field
+ */
 void mcmc_update_vectorfield_RWMH(mcmc_infchain *chain1, mcmc_infchain *chain2, double logLHDOfProposal);
-
 void randomPriorDrawOLD(gsl_rng *r, double PRIOR_ALPHA, fftw_complex *randDrawCoeffs);
-
 #endif
